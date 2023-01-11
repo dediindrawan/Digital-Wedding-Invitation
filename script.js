dcfd3f58c7803cf5fa3492d3b8aafd45e07b00e7
Dedi Indrawan <dediindrawan812@gmail.com>
2023-01-11 11:39:05 +0700

    Add Script to Run Count Down Wedding Time

--- script.js
+++ script.js
@@ -49,6 +49,8 @@ copyRekBtnBca.addEventListener('click', async () => {
     alert('Nomor Rekening Berhasil di Salin !!');
     const noRekBca = document.querySelector('.no-rek-bca').textContent;
     await navigator.clipboard.writeText(noRekBca);
+    const copied = navigator.clipboard.readText();
+    console.log(copied);
 });
 
 // create copy clipboard when user click button
@@ -58,6 +60,8 @@ copyRekBtnBni.addEventListener('click', async () => {
     alert('Nomor Rekening Berhasil di Salin !!');
     const noRekBni = document.querySelector('.no-rek-bni').textContent;
     await navigator.clipboard.writeText(noRekBni);
+    const copied = navigator.clipboard.readText();
+    console.log(copied);
 });
 
 // create copy clipboard when user click button
@@ -67,38 +71,41 @@ copyAddress.addEventListener('click', async () => {
     alert('Alamat Pengiriman Berhasil di Salin !!');
     const address = document.querySelector('.address').textContent;
     await navigator.clipboard.writeText(address);
+    const copied = navigator.clipboard.readText();
+    console.log(copied);
 });
 
 // set count down time remaining as real time
-const target = new Date('jan 22 2023 08:00:00').getTime();
+const target = new Date('Jan 22 2023 08:00:00').getTime();
 
-function countDown() {
+const countDown = setInterval(function () {
     const now = new Date().getTime();
-    let gap = target - now;
+    const gap = target - now;
 
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
+    let day = Math.floor(gap / (1000 * 60 * 60 * 24));
+    let hour = Math.floor(gap % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minute = Math.floor(gap % (1000 * 60 * 60) / (1000 * 60));
+    let second = Math.floor(gap % (1000 * 60) / 1000);
 
-    let days = Math.floor(gap / day);
-    let hours = Math.floor((gap % day) / hour);
-    let min = Math.floor((gap % hour) / minute);
-    let sec = Math.floor((gap % minute) / second);
+    day = day < 10 ? '0' + day : day;
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+    second = second < 10 ? '0' + second : second;
 
-    days = days < 10 ? '0' + days : days;
-    hours = hours < 10 ? '0' + hours : hours;
-    min = min < 10 ? '0' + min : min;
-    sec = sec < 10 ? '0' + sec : sec;
+    document.querySelector('.days p').innerHTML = `${day} <br> Hari`;
+    document.querySelector('.hours p').innerHTML = `${hour} <br> Jam`;
+    document.querySelector('.minutes p').innerHTML = `${minute} <br> Menit`;
+    document.querySelector('.seconds p').innerHTML = `${second} <br> Detik`;
 
-    document.querySelector('.days p').innerHTML = `${days} <br> Hari`;
-    document.querySelector('.hours p').innerHTML = `${hours} <br> Jam`;
-    document.querySelector('.minutes p').innerHTML = `${min} <br> Menit`;
-    document.querySelector('.seconds p').innerHTML = `${sec} <br> Detik`;
-
-    if (gap == 0) {
+    if (gap <= 0) {
         clearInterval(countDown);
-    };
-};
 
-setInterval(countDown, 1000);
+        document.querySelector('.day-remaining').innerHTML = `Hitung Mundur Selesai <br> Terimakasih Atas Kehadiran Anda`;
+
+        document.querySelector('.days p').innerHTML = `00 <br> Hari`;
+        document.querySelector('.hours p').innerHTML = `00 <br> Jam`;
+        document.querySelector('.minutes p').innerHTML = `00 <br> Menit`;
+        document.querySelector('.seconds p').innerHTML = `00 <br> Detik`;
+    };
+
+}, 1000);
